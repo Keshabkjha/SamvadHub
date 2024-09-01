792317faa5af6b33758643f76283987ee7b34341
Keshab Kumar <keshabkumarjha876@gmail.com>
2024-09-01 20:15:27 +0530

    Update send_code.php

--- assets/php/send_code.php
+++ assets/php/send_code.php
@@ -21,8 +21,8 @@ global $mail;
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'developerkeshab@gmail.com';                     //SMTP username
-        $mail->Password   = 'swgr upyx kenq rsal';                               //SMTP password
+        $mail->Username   = 'username';                     //SMTP username
+        $mail->Password   = 'password';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
